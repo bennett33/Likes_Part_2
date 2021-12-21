d80c7cea68ea1e4c517d8a51504c5fda0f08b3c5
bennett33 <blanzetta3@gmail.com>
2021-12-21 14:32:39 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var likes1 = 3;
+var likeElement = document.querySelector("#likes1");
+
+console.log(likeElement);
+
+function add1() {
+    likes1++;
+    likeElement.innerText = likes1 + " Like(s)";
+    console.log(likes1);
+}
+
+//------------//
+
+
+var likes2 = 12;
+var like2Element = document.querySelector("#likes2");
+
+console.log(like2Element);
+
+function add2() {
+    likes2++;
+    like2Element.innerText = likes2 + " Like(s)";
+    console.log(likes2);
+}
+
+
+//------------//
+
+
+var likes3= 9;
+var like3Element = document.querySelector("#likes3");
+
+console.log(like3Element);
+
+function add3() {
+    likes3++;
+    like3Element.innerText = likes3 + " Like(s)";
+    console.log(likes3);
+}
